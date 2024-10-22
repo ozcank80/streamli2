591184056a323980ca54a11e48b4273cabd5cfe2
Özcan Kurnaz <37580796+ozcank80@users.noreply.github.com>
2024-10-22 23:40:44 +0300

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -7,9 +7,17 @@ import streamlit as st
 
 st.title("Toksik insan mısın, değil misin??")
 
-st.image("./identify-toxic-people.jpg")
+st.image("./identify-toxic-people.jpg",width=300)
 
-st.header("Nereden nereye")
-st.markdown("Bu tarz yazilari kucultebiliyoruz..")
+st.markdown("Yaptığımız çalışmada kaggle dan yararlandık. Toksik veri setiyle çalıştık.")
 
-st.image("./cm5.png")
+st.image("./veri-1.png")
+st.markdown("Veri setimizde aşağıdaki şekilde sadeleştirme yaptık.")
+st.image("./veri-2.png")
+st.image("./veri-3.png")
+st.image("./veri-4.png")
+st.markdown("sadeleştirme sonrasında lntk kütüphanesinden yararlanarak modelleme çalışması öncesi verimizin son halini verdik.")
+st.image("./bir.png")
+st.image("./iki.png")
+st.markdown("vektorizasyon ve modelleri kullarak çalışma yaptık. Aşağıda kullandığımız modellerin confision matriklerini görebilirsiniz.")
+
