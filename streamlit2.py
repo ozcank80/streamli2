9fdfb456af391142d1f2e4916ecde63cf323d9c6
Özcan Kurnaz <37580796+ozcank80@users.noreply.github.com>
2024-10-22 23:23:42 +0300

    Update streamlit2.py

--- streamlit2.py
+++ streamlit2.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("Deneme amaçlı oluşturuldu")
+#from nltk.corpus import stopwords
+
+#from nltk.stem import WordNetLemmatizer, SnowballStemmer
+#from nltk.tag import pos_tag
+
+st.title("Toksik insan mısın, değil misin??")
+
+st.image('./identify-toxic-people.jpg',width=250)
 
 st.header("Nereden nereye")
 st.markdown("Bu tarz yazilari kucultebiliyoruz..")
